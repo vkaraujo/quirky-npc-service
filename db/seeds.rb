33c4f603cc08c13062147fd18aa553524805cca0
viktor <araujo>
2025-03-26 01:53:29 -0300

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+
+require 'faker'
+
+Npcs = []
+
+NAMES = [
+  "Mibzle Gork", "Tilda Fizzlepot", "Brum the Quiet", "Zarnak Doomwhisper",
+  "Pip Thimbletoe", "Glimmer Fernwhistle", "Snorgus the Flatulent", "Ellie of the Endless Tea"
+]
+
+JOBS = [
+  "wand polisher", "dragon dentist", "potion tester", "bard critic",
+  "sock merchant", "wandering poet", "goblin negotiator", "beholder stylist"
+]
+
+QUIRKS = [
+  "only speaks in rhymes", "thinks they're invisible", "collects spoons",
+  "scared of butterflies", "believes they're a potato", "hums constantly",
+  "obsessed with ducks", "always narrating their actions"
+]
+
+MOODS = [
+  "mysteriously excited", "deeply confused", "overly confident", "melancholic but hopeful",
+  "suspiciously friendly", "dramatically bored", "quietly panicked", "whimsically angry"
+]
+
+20.times do
+  Npcs << Npc.create!(
+    name: NAMES.sample,
+    job: JOBS.sample,
+    quirk: QUIRKS.sample,
+    mood: MOODS.sample
+  )
+end
+
+puts "🌟 Seeded #{Npcs.count} NPCs!"

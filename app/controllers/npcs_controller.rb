0a73043e16fa8f1b4d885749343c39f82699a91b
viktor <araujo>
2025-03-26 11:36:19 -0300

    pagination added with pagy gem

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -1,7 +1,15 @@
 class NpcsController < ApplicationController
   def index
-    @npcs = Npc.all
-    render json: @npcs
+    @pagy, @npcs = pagy(Npc.all)
+    render json: {
+      npcs: ActiveModelSerializers::SerializableResource.new(@npcs),
+      pagination: {
+        page: @pagy.page,
+        items: @pagy.items,
+        pages: @pagy.pages,
+        count: @pagy.count
+      }
+    }
   end
 
   def show
@@ -21,34 +29,34 @@ class NpcsController < ApplicationController
 
   def create
     @npc = Npc.new(npc_params)
-  
+
     if @npc.save
       render json: @npc, status: :created
     else
       render json: { errors: @npc.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
   def update
     @npc = Npc.find(params[:id])
-  
+
     if @npc.update(npc_params)
       render json: @npc
     else
       render json: { errors: @npc.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
   def destroy
     @npc = Npc.find(params[:id])
     @npc.destroy
-  
+
     head :no_content
   end
-  
+
   private
-  
+
   def npc_params
     params.require(:npc).permit(:name, :job, :quirk, :mood)
-  end  
+  end
 end
